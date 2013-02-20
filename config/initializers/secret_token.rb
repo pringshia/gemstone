aa4f1ce944036fac5eb858db1bd57e2850980aaa
pringshia <pratik@ringshia.com>
2013-02-19 21:50:59 -0500

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemstone::Application.config.secret_token = '5a661f832b886005214d385720e028d29379ee1ebea4ad90b2866cbac800d281a5144409f1baab51a717cb3e68cf2899e217b146401005386b74a8c4dccabdcb'
