58d8f8b1bfc3e2ec386b7327c2fc5121c9123f52
pringshia <pratik@ringshia.com>
2013-02-22 01:06:51 -0500

    Correct Aaron's foolishness; remove extraneous code from migration

--- db/migrate/20130222040524_create_users.rb
+++ db/migrate/20130222040524_create_users.rb
@@ -1,16 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
-
-	set primary_key :id
-
   def change
     create_table :users, { :id => false, :primary_key => :id } do |t|
       t.integer :id, :limit => 8
 
       t.timestamps
     end
-  end
-
-  def to_param
-  	:id
   end
 end
