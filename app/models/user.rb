85f17ec5c703b284e2d9804ccad07eff72aa467a
Aaron <aaron@will-hp-desktop.(none)>
2013-02-25 00:32:19 -0500

    Fixed Facebook Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(auth.slice(:id)).first_or_initialize.tap do |user|
-      user.oauth_token = auth.credentials.token
-      user.name = auth.info.name
-      user.id = auth.uid
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+        where(:id => auth.uid).first_or_initialize.tap do |user|
+        user.oauth_token = auth.credentials.token
+        user.name = auth.info.name
+        user.id = auth.uid
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.save!
     end
   end
 end
