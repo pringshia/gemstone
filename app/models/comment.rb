c70392f3372ecab5f40bd1219fef2581304ffe08
Aaron <aaron@will-hp-desktop.(none)>
2013-03-05 23:03:32 -0500

    Corrected Errors from Last Push

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,17 +3,14 @@ class Comment < ActiveRecord::Base
 	belongs_to :picture, :counter_cache => true
 	has_many :ratings
 	set_primary_key :id
-    attr_accessible :id, :user_id, :picture_id, :comment_text
-    attr_readonly :ratings_count
+    attr_accessible :id, :user_id, :picture_id, :comment_text, :ratings_count
 
     def self.find_best_for(user)
     user = (user.class == User)? user.id : user
     find(:all, 
          :joins => ["LEFT OUTER JOIN ratings ON ratings.comment_id = comments.id"], 
-         :select => "COUNT(1) ratings_count, comments.picture_id, comments.comment_text, comments.id"
-         :group => "comments.id",
-         :conditions => ["(comments.id NOT IN (SELECT comment_id FROM ratings WHERE ratings.user_id = ? )) AND comments.user_id <> ?", user, user], 
-         :order => "ratings_count ASC", 
+         :conditions => ["(comments.id NOT IN (SELECT ratings.comment_id FROM ratings WHERE ratings.user_id = ? )) AND comments.user_id <> ?", user, user], 
+         :order => "comments.ratings_count ASC", 
          :limit => 1)
 
   end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,6 @@
 class Rating < ActiveRecord::Base
-  belongs_to :picture
-  belongs_to :user, :counter_cache => true
+  belongs_to :user
+  belongs_to :comment, :counter_cache => true
   set_primary_key :id
   attr_accessible :id, :user_id, :comment_id, :is_positive
 end
