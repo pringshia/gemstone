f68cf27d1b97c1565307915806c0e6b42a8f89b7
Aaron <aaron@will-hp-desktop.(none)>
2013-04-14 16:57:49 -0400

    reorder pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
 
   def index
   	user = User.find(session[:user_id])
-    @pictures = user.pictures    
+    @pictures = user.pictures.order("created_at")    
     
     respond_to do |format|
       format.html # index.html.erb
