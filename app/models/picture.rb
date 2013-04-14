214baef6553f21398f49f1e308b9091b715605a4
Aaron <aaron@will-hp-desktop.(none)>
2013-04-14 17:15:38 -0400

    Fixed Commenting again

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -9,9 +9,9 @@ class Picture < ActiveRecord::Base
   def self.find_best_for(user)
     user = (user.class == User)? user.id : user
     find(:all, 
-         :joins => "LEFT OUTER JOIN comments ON comments.picture_id = pictures.id",
+         :joins => "LEFT OUTER JOIN comments ON comments.picture_id = pictures.id", 
          :conditions => ['(pictures.id NOT IN (SELECT comments.picture_id FROM comments WHERE comments.user_id = ?)) AND pictures.user_id <> ?', user, user], 
-         :group => 'pictures.id, pictures.link, pictures.comments_count',
+         :group => 'pictures.id, pictures.link, pictures.comments_count', 
          :order => "comments_count ASC", 
          :limit => 3)
   end
